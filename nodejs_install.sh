ae8abd9328a453524482007b73ff09f3e2d62708
SDCOM <q6hqAsJ89h3Z8U2eF6AK8D+SDCOM@noreply.cnb.cool>
2025-05-01 22:10:50 +0800

    chore: 修改 nvm 安装脚本的下载地址
    将 nvm 安装脚本的下载地址从原 GitHub 地址修改为通过 gh.sdcom.asia 代理的地址

--- nodejs_install.sh
+++ nodejs_install.sh
@@ -1,5 +1,5 @@
 # Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+curl -o- https://gh.sdcom.asia/https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 # in lieu of restarting the shell
 \. "$HOME/.nvm/nvm.sh"
 # Download and install Node.js:
