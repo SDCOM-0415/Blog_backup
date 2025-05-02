88ff3b17a9e1ffe5ea0db4bfe645353607508f90
SDCOM <q6hqAsJ89h3Z8U2eF6AK8D+SDCOM@noreply.cnb.cool>
2025-05-02 11:26:01 +0800

    删除文件 nodejs.sh

--- nodejs.sh
+++
@@ -1,8 +0,0 @@
-# Download and install Node.js:
-fnm install 22
-# Verify the Node.js version:
-node -v # Should print "v22.15.0".
-# Verify npm version:
-npm -v # Should print "10.9.2".
-# Mirror
-npm config set registry https://mirrors.cloud.tencent.com/npm/
